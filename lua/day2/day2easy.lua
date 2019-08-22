f285af75c42c3af5ed54cada479f3c11184abc2e
Brent <brentatkins@gmail.com>
2019-08-22 22:02:43 +0200

    lua day2 easy

---
+++ lua/day2/day2easy.lua
@@ -0,0 +1,52 @@
+function print_table(t)
+    for k, v in pairs(t) do
+        print(k .. ", " .. v)
+    end
+end
+
+function concatenate(a1, a2)
+    local result = {}
+    local maxa1 = 0
+    for i, v in pairs(a1) do
+        result[i] = v
+        maxa1 = i
+    end
+
+    for i, v in pairs(a2) do
+        result[i + maxa1] = v
+    end
+
+    return result
+end
+
+-- test1 = concatenate({"a","b","c"}, {"d","e","f"})
+-- print_table(test1) 
+
+---------------------------------------------------------
+
+local _private = {}
+
+function strict_read(table, key)
+    if _private[key] then
+        return _private[key]
+    else
+        error("Invalid key: " .. key)
+    end
+end
+
+function strict_write(table, key, value)
+    if _private[key] and not value == nil then
+        error("Duplicate key: " .. key)
+    else
+        _private[key] = value
+    end
+end
+
+local mt = {
+    __index = strict_read,
+    __newindex = strict_write
+}
+
+treasure = {}
+setmetatable(treasure, mt)
+
